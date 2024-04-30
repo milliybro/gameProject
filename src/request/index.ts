bedaada039845b2b91746c63ed72f4ede9ea9184
“milliybro” <shohrux-rustamov@mail.ru>
2024-04-30 09:48:29 +0500

    ENDPOINT changed

--- src/request/index.ts
+++ src/request/index.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 import { TOKEN } from "../constants";
 
 export const request = axios.create({
-  baseURL: "http://13.60.23.197:8000/api/v1/",
+  baseURL: "https://pythonjon.pythonanywhere.com/api/v1/",
   timeout: 10000,
   headers: {
     Authorization: Cookies.get(TOKEN) ? "Bearer " + Cookies.get(TOKEN) : null
